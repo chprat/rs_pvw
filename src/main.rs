4e943dd8cc86dfe3bf8561d3f46d82ea91975ad8
chprat <49040338+chprat@users.noreply.github.com>
2020-02-28 20:41:39 +0100

    Add GTK hello world

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 use std::env;
+extern crate gtk;
+use gtk::prelude::*;
+use gtk::{ButtonsType, DialogFlags, MessageDialog, MessageType, Window};
 
 fn slider() {
-    println!("Slider")
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+    MessageDialog::new(
+        None::<&Window>,
+        DialogFlags::empty(),
+        MessageType::Info,
+        ButtonsType::Ok,
+        "Hello World",
+    )
+    .run();
 }
 
 fn move_files() {
